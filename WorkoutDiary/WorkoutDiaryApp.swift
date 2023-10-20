e0db6d46207a297d72587ad8c873015d53f662df
KimberleyMatthews <kimberleyahnlundmatthews@gmail.com>
2023-10-20 11:02:09 +0200

    Project started
    Project just started, no adjustments done.

---
+++ WorkoutDiary/WorkoutDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutDiaryApp.swift
+//  WorkoutDiary
+//
+//  Created by STI Mac 9 on 2023-10-20.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
